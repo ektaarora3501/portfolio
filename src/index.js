94ee44cdd5e37925f6ac323d77319bc396068305
Ekta Arora <42875737+ektaarora3501@users.noreply.github.com>
2019-12-16 18:24:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 //ReactDOM.render(<Sidebar />, document.getElementById('root'));
 
 
-serviceWorker.register();
+serviceWorker.unregister();
